55c00fa2e44602a25ce9c70d6547708715429643
MichaelFelixChan <michaelfelixchandra_2327240030@mhs.mdp.ac.id>
2025-11-21 16:20:02 +0700

    backend-housing

---
+++ griya-mdp/griya-mdp-backend/app_server/models/db.js
@@ -0,0 +1,17 @@
+let mongoose = require("mongoose");
+let dbURI = "mongodb://localhost:27017/paw2-si5c";
+
+mongoose.connect(dbURI,{});
+
+mongoose.connection.on("connected", () => {
+    console.log("Connected To MongoDB");
+});
+
+mongoose.connection.on("error", (error) => {
+    console.log("Connection Error: " + error);
+});
+
+mongoose.connection.on("disconected", () => {
+    console.log("Disconnected From MongoDB");
+});
+
