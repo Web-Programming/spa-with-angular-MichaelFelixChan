732cd56011fb27a7f5692d6b1618f96a2a31896d
MichaelFelixChan <michaelfelixchandra_2327240030@mhs.mdp.ac.id>
2025-12-01 18:12:24 +0700

    first

--- griya-mdp/src/app/app.ts
+++ griya-mdp/src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Auth } from './services/auth';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('griya-mdp');
+
+  // Inject AuthService dan Router
+  constructor(public authService: Auth, private router: Router) {}
+
+  // Method logout
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
